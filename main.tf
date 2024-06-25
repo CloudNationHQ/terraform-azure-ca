2180d61b60548e0868ddf012caffa9e89dcb8d80
Eddy Vera <eddy.vera@cloudnation.nl>
2024-06-25 22:16:23 +0200

    fix: update identity property of secret block

--- main.tf
+++ main.tf
@@ -291,7 +291,7 @@ resource "azurerm_container_app" "ca" {
     content {
       name                = secret.value.name
       value               = try(secret.value.value, null)
-      identity            = try(secret.value.value, null) == null ? coalesce(secret.value.identity, azurerm_user_assigned_identity.identity[secret.value.id_name].id) : null
+      identity            = try(secret.value.key_vault_secret_id, null) == null ? null : coalesce(secret.value.identity, azurerm_user_assigned_identity.identity[secret.value.id_name].id)
       key_vault_secret_id = try(secret.value.key_vault_secret_id, null)
     }
   }
